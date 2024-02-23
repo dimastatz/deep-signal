410cc41dd03c30c94b5f2ccc630c34f345a5b8fe
dimastatz <dima.statz@gmail.com>
2024-02-23 09:32:14 +0200

    add readme

--- deep-signal/src/test/scala/org/apache/deepsignal/TestMicroBatch.scala
+++ deep-signal/src/test/scala/org/apache/deepsignal/TestMicroBatch.scala
@@ -1,11 +1,13 @@
 package org.apache.deepsignal
 
-import org.apache.deepsignal.whisper.MicroBatch
+import java.nio.file.Paths
 import org.scalatest.funsuite.AnyFunSuite
+import org.apache.deepsignal.whisper.MicroBatch
 
 
 class TestMicroBatch extends AnyFunSuite{
     test("testLoadModel") {
+        println(Paths.get(".").toAbsolutePath)
         val tryLoadModel = MicroBatch.loadModel("", "")
         assert(tryLoadModel.isFailure)
     }
