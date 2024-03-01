cd9a676982b15903967021e1c76b80b6dc44d360
dimastatz <dima.statz@gmail.com>
2024-03-01 21:19:18 +0200

    annotation

--- python/deepsignal/transcription/whisper_wrapper.py
+++ python/deepsignal/transcription/whisper_wrapper.py
@@ -1,9 +1,11 @@
 """ integration of whisper """
+import typing as tp
+
 import whisper
 import numpy as np
 
 
-def get_transcriber():
+def get_transcriber() -> tp.Callable[[np.ndarray], str]:
     """create whisper transcriber"""
     model = whisper.load_model("base")
 
