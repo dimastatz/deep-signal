4225cfa143699569624019b4c97572e46c3f75b2
dimastatz <dima.statz@gmail.com>
2024-03-01 10:56:05 +0200

    add transcribe

--- python/tests/test_transcribe.py
+++ python/tests/test_transcribe.py
@@ -1,8 +1,24 @@
 """ test transcriptions """
+import os
+import difflib as df
+import librosa as lr
 import deepsignal.transcription.whisper_wrapper as whisper
 
 
 def test_whisper_transcribe():
     """test whisper transcribe"""
+    expected = (
+        " or was of heaven mine. Thus was then she."
+        + " What as-be said Sarah, as to Lady for better, very stob."
+    )
+
     transcriber = whisper.get_transcriber()
     assert not transcriber is None
+
+    path = os.getcwd() + "/tests/resources/sample-4.mp3"
+    buffer, _ = lr.load(path)
+    result = transcriber(buffer)
+    print(1, result["text"])
+    print(2, expected)
+
+    assert df.SequenceMatcher(None, expected, result["text"]).ratio() > 0.9
