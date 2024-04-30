2c569e5cc78da7e3f44ef2802e1a3b98921ba031
Dima Statz <dima.statz@gmail.com>
2024-04-30 22:01:08 +0300

    fix transcribe file

---
+++ python/deepsignal/client/transcribe_file.py
@@ -0,0 +1,32 @@
+import time
+import wave
+import whisper
+from multiprocessing import Pipe, Process
+from multiprocessing.connection import Connection
+
+
+def run_server(con: Connection, model: whisper.Whisper):
+    frames = []
+    while True:
+        if con.poll():
+            frames.append(con.recv())
+        else:
+            time.sleep(0.1)
+            print(f"{time.time()}: Frames {len(frames)}")
+
+
+def run_client(con: Connection, fname: str):
+    print(f"{time.time()}: Client")
+    pass
+
+
+if __name__ == "__main__" :
+    con1, con2 = Pipe()
+    model = whisper.load_model("base")
+
+    proc = Process(target=run_server, args=(con2,model,),)
+   
+    proc.start()
+    run_client(con1, "")
+    time.sleep(1)
+    proc.terminate()
