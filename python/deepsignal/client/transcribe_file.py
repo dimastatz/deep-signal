a8d7e80b9933e33c5a7de9b22ab5c32e3827c758
Dima Statz <dima.statz@gmail.com>
2024-05-02 16:09:15 +0300

    add file loc

--- python/deepsignal/client/transcribe_file.py
+++ python/deepsignal/client/transcribe_file.py
@@ -1,6 +1,7 @@
 import time
 import wave
 import whisper
+from pathlib import Path
 from multiprocessing import Pipe, Process
 from multiprocessing.connection import Connection
 
@@ -23,6 +24,10 @@ def run_client(con: Connection, fname: str):
 if __name__ == "__main__":
     con1, con2 = Pipe()
     model = whisper.load_model("base")
+    path = Path(__file__).parent.absolute()
+    
+    print(path)
+
 
     proc = Process(
         target=run_server,
